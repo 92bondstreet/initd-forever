9bb05bdf0e74eaa1d448ab579ec549046e3c1f83
Yassine Azzout <92bondstreet@gmail.com>
2013-09-27 14:26:08 +0200

    TDD

---
+++ test/forever.js
@@ -0,0 +1,134 @@
+(
+	function()
+	{
+		var should = require('should');
+		var daemon = require('../lib/main');
+
+		var traceError = function (error, retval) {
+		  if (error) {
+		    console.log(error);
+		    return;
+		  }
+		  console.log(retval);
+		}
+
+
+ 	   /*
+ 		* 		Generate 
+ 		*/
+ 		describe('Generate daemon script file', function() {
+ 			describe('with no arguments', function() {
+ 				it('throw ERROR', function() {
+		          (function () {
+		          	daemon.script();		            
+		          }).should.throw();  
+		        });
+		    });
+
+		    describe('More than 2 invalids arguments', function() {
+		    	it('throw ERROR', function() {
+		          (function () {
+		          	daemon.script("","");		
+		          }).should.throw();
+		        });
+
+		        it('throw ERROR', function() {
+		          (function () {
+		          	daemon.script("","","");		
+		          }).should.throw();
+		        });
+
+		         it('throw ERROR', function() {
+		          (function () {
+		          	daemon.script("","","","");		
+		          }).should.throw();
+		        });
+		    });
+
+		    describe('with 2 valids arguments', function() {
+ 			
+		        it('check if package.json is missing ', function() {
+		        	daemon.PackageInfo.should.have.property('info',false);			        	
+		        });
+
+		        it('check if package.json has minimalistic values', function() {
+		        	daemon.PackageInfo.should.have.property('info',true);	
+		        	daemon.PackageInfo.should.have.property('name');
+		        	daemon.PackageInfo.should.have.property('appjs');
+		        });		       
+		    });
+
+		    describe('with 2 valids arguments', function() {
+		    	describe('with no options', function() {
+
+		    		it('Callback error if package.json is missing', function(done) {
+		    			             						         	
+			        	daemon.script([],function(err,response){
+	             			if (err) 
+	             				console.log(err);	             		
+	            			done();
+	          			});
+		    		});
+
+		    		it('Callback error if package.json has not minimalistic info', function(done) {
+		    			               						         	
+			        	daemon.script([],function(err,response){
+	             			if (err) 
+	             				console.log(err);	             		
+	            			done();
+	          			});
+		    		});
+
+		    		it('generate script file with package.json info', function(done){
+			        	               						         	
+			        	daemon.script([],function(err,response){
+	             			if (err) 
+	             				console.log(err);
+	             			else
+	             				console.log(response);
+	            			done();
+	          			});
+					});
+
+
+		    	});
+
+		    	describe('with options', function() {
+		    		
+		    		it('Callback error if -a, -n are missing AND package.json is missing', function(done) {
+		    			             						         	
+			        	daemon.script(['npm','test','-e','production'],function(err,response){
+	             			if (err) 
+	             				console.log(err);	             		
+	            			done();
+	          			});
+		    		});
+
+		    		it('generate script file with arguments', function(done) {
+		    			           						         	
+			        	daemon.script(['npm', 'test', '-a', '/var/www/mynodesite/app.js', '-e', 'production', '-n', 'mynodesite'],function(err,response){
+	             			if (err) 
+	             				console.log(err);	
+	             			else
+	             				console.log(response);	            		
+	            			done();
+	          			});
+		    		});
+
+		    		it('generate monit script file with arguments', function(done) {
+		    			             						         	
+			        	daemon.script(['npm', 'test', '-a', '/var/www/mynodesite/app.js', '-e', 'production', '-n', 'mynodesite', '-m', '3000'],function(err,response){
+	             			if (err) 
+	             				console.log(err);	
+	             			else
+	             				console.log(response);	            		
+	            			done();
+	          			});
+		    		});
+		    	});
+		    });
+
+
+ 		});
+	}
+)();
